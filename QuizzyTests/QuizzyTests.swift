e4e931ef9d4a7e6314f1c93fbcc492fdf9d19d63
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-01-28 13:12:17 +0300

    Initial Commit

---
+++ QuizzyTests/QuizzyTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzyTests.swift
+//  QuizzyTests
+//
+//  Created by Muhammed Mustafa Geldi on 28.01.2025.
+//
+
+import Testing
+@testable import Quizzy
+
+struct QuizzyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
